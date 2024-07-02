4d59e352bb20fe32029cc0e90f2ace416fc00c9f
kapilan1 <kapilan@codelantic.com>
2024-07-02 11:28:34 +0530

    hasing + bcrypt( salting)

--- securityDemo/src/main/java/com/example/securityDemo/Config/SecurityConfig.java
+++ securityDemo/src/main/java/com/example/securityDemo/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.securityDemo.Config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,8 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,6 +28,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity      // enable web security to access in this application
 @EnableMethodSecurity       //enables method-level security to this application.
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
     @Bean
         // mark as spring bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -52,18 +61,31 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")                        //{noop} is to encrypt password
+                .password(passwordEncoder().encode("password1"))
                 .roles("USER")
                 .build();
 
         UserDetails admin1 = User.withUsername("admin")
-                .password("{noop}adminPassword")                        //{noop} is to encrypt password
+                .password(passwordEncoder().encode("adminPassword"))
                 .roles("ADMIN")
                 .build();
 
-        // test using in memory data( no need for database)
+        // test using in memory data( no need for database), here these user1, admin1 account won't be created in database
         // InMemoryUserDetailsManager needs object of type 'UserDetails' as arguments
         // we can pass any number of objects as arguments
-        return new InMemoryUserDetailsManager(user1,admin1);
+//        return new InMemoryUserDetailsManager(user1,admin1);
+
+
+
+        // but here a new record will be created to the database
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user1);
+        jdbcUserDetailsManager.createUser(admin1);
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
