7aa89de8e4440365081e3fff079109136771ebc1
kapilan1 <kapilan@codelantic.com>
2024-07-12 14:38:10 +0530

    necessary comments added

--- securityDemo/src/main/java/com/example/securityDemo/jwt/JwtUtils.java
+++ securityDemo/src/main/java/com/example/securityDemo/jwt/JwtUtils.java
@@ -31,33 +31,34 @@ public class JwtUtils {
     private String jwtExpirationMs;
 
     public String getJwtFromHeader(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
+        String bearerToken = request.getHeader("Authorization");    //Retrieves the value of the "Authorization" header from the HTTP request
         log.info("bearerToken = " + bearerToken);
         loggger.debug("Authorization header :{} ", bearerToken);
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);   // to remove Bearer with space(first 7 characters with space contained)
+            return bearerToken.substring(7);   // remove Bearer with space(first 7 characters with space contained) and give remaining
         }
         return null;
     }
 
     public String generateTokenFromUserName(UserDetails userDetails) {
         String userName = userDetails.getUsername();
-        return Jwts.builder()
+        return Jwts.builder()           // create JWT token using the Jwts builder.
                 .subject(userName)
-                .issuedAt(new Date())
+                .issuedAt(new Date())      // current date and time as token generated
                 .expiration(new Date(new Date().getTime() + jwtExpirationMs))
                 .signWith(key())
-                .compact();
+                .compact();     // Builds and serializes the JWT token into a compact, URL-safe string.
     }
 
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parser()
-                .verifyWith((SecretKey) key()).build()
-                .parseSignedClaims(token)
-                .getPayload().getSubject();
+                .verifyWith((SecretKey) key()).build()      //Verifies the token with the secret key obtained from the key() method and builds the parser.
+                .parseSignedClaims(token)         //Parses the signed claims (the payload) from the JWT token.
+                .getPayload().getSubject();     //Retrieves the subject (username) from the payload and returns
     }
 
     private Key key() {
+        // Decodes the jwtSecret from Base64 and returns an HMAC SHA key for signing the JWT tokens.
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
